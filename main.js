4f10053f1a006428261f9a05117971627adf009f
jamalsheik46008 <jamalsheik383@gmail.com>
2023-04-13 08:16:29 +0530

    add

---
+++ main.js
@@ -0,0 +1,61 @@
+document.write("dell");
+console.log("weclome");
+//alert("thanq");
+
+
+//(variables)
+
+console.log(a);
+var a=4; var b=8;
+var a=b+a;
+console.log(a);
+
+//(variables)
+ 
+let y=5; let z=10;
+let x=z-y;
+console.log(y);
+
+
+//(variables)
+
+const t=6,u=3;
+const n=t+u;
+console.log(n);
+
+
+//(variables)
+
+d =7,j =4;
+v=d+j;
+console.log(v);
+
+
+//(together)
+
+var k=5;
+let f=4;
+const o=7;
+k=f+o;
+console.log(k);
+
+
+// (variables)
+
+let i="5";
+console.log(typeof i);
+
+
+let e=4;
+console.log(typeof e);
+
+var name="AlexRaja";
+console.log(typeof name);
+
+
+
+
+
+
+
+
